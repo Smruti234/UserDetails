da756e02fd15ca7b390bb3b2206ed1fa6ec3a0f2
Smruti <lenkasmruti123@gmail.com>
2021-10-28 22:03:43 +0530

    Added: Uc1_LastName

--- com/uservalidation/UserDetail.java
+++ com/uservalidation/UserDetail.java
@@ -3,10 +3,10 @@ package com.uservalidation;
 import java.util.regex.Pattern;
 
 public class UserDetail {
-    private final static String FirstName = "([A-Z]{1}[a-zA-Z]{2,})$";
-
-    public boolean validateFirstName(String fName){
-        Pattern pattern = Pattern.compile(FirstName);
-        return pattern.matcher(fName).matches();
+    	public boolean lastName(String name) {
+            boolean mathPattern = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", name);
+            return mathPattern;
+        }
     }
-}
+    
+    
--- com/uservalidation/UserDetailsTest.java
+++ com/uservalidation/UserDetailsTest.java
@@ -1,27 +1,15 @@
 package com.uservalidation;
 
 import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
 import org.junit.jupiter.api.Assertions;
 
 public class UserDetailsTest {
-    @Test
-    void givenFirstName_CheckValidation_ReturnTrue() {
-        UserDetail userDetails = new UserDetail();
-        boolean result = userDetails.validateFirstName("SMRUTI");
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    void givenFirstLetterSmall_CheckForValidation_ReturnFalse() {
-        UserDetail userDetails = new UserDetail();
-        boolean result = userDetails.validateFirstName("smruti");
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    void givenTwoLetters_CheckForValidation_ReturnFalse() {
-        UserDetail userDetails = new UserDetail();
-        boolean result = userDetails.validateFirstName("sm");
-        Assertions.assertFalse(result);
-    }
-}
+	 @Test
+	   public void givenNameWhenPropershouldReturnTrue(){
+		 UserDetailsTest userDetails = new UserDetailsTest();
+	         boolean testStatus = userDetails.lastName("Lenka");
+	         Assert.assertTrue(testStatus);
+	   }
