017c2d24d71a95c43e80ce5f283603fce3f03623
Smruti <lenkasmruti123@gmail.com>
2021-10-28 23:40:12 +0530

    Added:Uc10_PasswordTest

--- com/uservalidation/UserDetailsTest.java
+++ com/uservalidation/UserDetailsTest.java
@@ -5,10 +5,38 @@ import org.junit.jupiter.api.Test;
 
 public class UserDetailsTest {
 	 private Assertions Assert;
-	  @Test
-	   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne(){
-		  UserDetail userDetails = new UserDetail();
-	         boolean testStatus = userDetails.gmailTest("abc+100@gamil.com");
-	         Assert.assertTrue(testStatus);
+	 @Test
+	   public void givenFirstNameWhenPropershouldReturnTrue() {
+	      UserDetail userDetails = new UserDetail();
+	      boolean testStatus = userDetails.firstName("Smruti");
+	      Assert.assertTrue(testStatus);
+	   }
+
+	   @Test
+	   public void givenLastNameWhenPropershouldReturnTrue() {
+	      UserDetail userDetails = new UserDetail();
+	      boolean testStatus = userDetails.lastName("Lenka");
+	      Assert.assertTrue(testStatus);
+	   }
+
+	   @Test
+	   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne() {
+	      UserDetail userDetails = new UserDetail();
+	      boolean testStatus = userDetails.passwordTest("Smruti@345");
+	      Assert.assertTrue(testStatus);
+	   }
+
+	   @Test
+	   public void givenEmailShouldReturnTrueWhenalltheConditionSatisfied() {
+	      UserDetail userDetails = new UserDetail();
+	      boolean testStatus = userDetails.gmailTest("Smruti@gdf567");
+	      Assert.assertTrue(testStatus);
+	   }
+
+	   @Test
+	   public void givenMessageShouldReturnSadWhenTestIsPassed(){
+	      MoodAnalyser mood =  new MoodAnalyser();
+	      String testStatus = mood.messageContainHappyReturnHappyElseSad("This is Sad message");
+	      Assert.assertEquals("Sad",testStatus);
 	   }
 	}
