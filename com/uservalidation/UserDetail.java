c8331804ba80049681468ce15da3fc0049f9948a
Smruti <lenkasmruti123@gmail.com>
2021-10-28 23:51:32 +0530

    Added:Uc11_ParameterizedTest

--- com/uservalidation/UserDetail.java
+++ com/uservalidation/UserDetail.java
@@ -1,41 +1,15 @@
 package com.uservalidation;
 
 import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class UserDetail {
-	 private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@]{1}[a-zA-Z0-9]+[.a-z]{4}([.a-zA-Z]{3})*$";
-	    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
-	    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
-	    private static final String MOBILE_NO_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
-	    private static final String PASSWORD_PATTERN = "([!@#$%^&*()]?[A-Za-z0-9]+){8,}$";
+	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@]{1}[a-zA-Z0-9]+[.a-z]{3,4}([.a-zA-Z]{3})*$";
 
-	    public boolean firstName(String firstName) {
-	        boolean mathPattern = Pattern.matches(FIRST_NAME_PATTERN, firstName);
-	        return mathPattern;
-	    }
-
-	    public boolean lastName(String lastName) {
-	        boolean mathPattern = Pattern.matches(LAST_NAME_PATTERN, lastName);
-	        return mathPattern;
-	    }
-
-	    public boolean mobileNumber() {
-	        boolean matchesString = Pattern.matches(MOBILE_NO_PATTERN, "7504614661");
-	        return matchesString;
-	    }
-
-	    public boolean passwordTest(String password) {
-	        boolean matchesString = Pattern.matches(PASSWORD_PATTERN, password);
-	        return matchesString;
-	    }
-	    public boolean gmailTest(String email) {
-	        boolean matchesString = Pattern.matches(EMAIL_PATTERN, email);
-	        return matchesString;
-	    }
-
-		public String messageContainHappyReturnHappyElseSad(String string) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+    public static boolean gmailTest(String email, boolean actualValue) {
+        boolean matchesString = Pattern.matches(EMAIL_PATTERN, email);
+        if(matchesString == actualValue)
+            return actualValue;
+        else
+            return false;
+    }
 }
--- com/uservalidation/UserDetailsTest.java
+++ com/uservalidation/UserDetailsTest.java
@@ -1,42 +1,54 @@
 package com.uservalidation;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+
+@RunWith(value = Parameterized.class)
 public class UserDetailsTest {
-	 private Assertions Assert;
-	 @Test
+	 private  UserDetail validator;
+	   private static String emailPattern;
+	   private static boolean expectedResult;
+
+	   public void UserDetail(String emailPattern, boolean expectedResult){
+	      this.emailPattern = emailPattern;
+	      this.expectedResult = expectedResult;
+	   }
+
+	   @Before
+	   public void insilized(){
+	      validator = new UserDetail();
+	   }
+	   @Parameterized.Parameters(name= "{index}: isValid({0})={1}")
+	   public static Iterable<Object[]> data() {
+	      return Arrays.asList(new Object[][]{
+	                      {"abc-100@yahoo.com", true},
+	                      {"abc.100@yahoo.com", true},
+	                      {"abc111@gmail.com", true},
+	                      {"abc-100@abc.com", true},
+	                      {"abc-100@abc.com", true},
+	                      {"abc@gmail.com.com", true},
+	                      {"abc@gmail.com.com", true},
+	                      {"abc@1.com", true},
+	                      {"abc+100@gmail.com", true},
+	                      {"abc", false},
+	                      {".abc+100@gmail.com", false},
+	                      {"abc+100@gmail.com", false},
+	                      {"abc+100@gmail.a", false},
+	                      {"abc+100@.gmail.com", false},
+	                      {"abc+100*()@gmail.com", false}
+	              }
+	      );
+	   }
+
+	   @Test
 	   public void givenFirstNameWhenPropershouldReturnTrue() {
-	      UserDetail userDetails = new UserDetail();
-	      boolean testStatus = userDetails.firstName("Smruti");
-	      Assert.assertTrue(testStatus);
-	   }
-
-	   @Test
-	   public void givenLastNameWhenPropershouldReturnTrue() {
-	      UserDetail userDetails = new UserDetail();
-	      boolean testStatus = userDetails.lastName("Lenka");
-	      Assert.assertTrue(testStatus);
-	   }
-
-	   @Test
-	   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne() {
-	      UserDetail userDetails = new UserDetail();
-	      boolean testStatus = userDetails.passwordTest("Smruti@345");
-	      Assert.assertTrue(testStatus);
-	   }
-
-	   @Test
-	   public void givenEmailShouldReturnTrueWhenalltheConditionSatisfied() {
-	      UserDetail userDetails = new UserDetail();
-	      boolean testStatus = userDetails.gmailTest("Smruti@gdf567");
-	      Assert.assertTrue(testStatus);
-	   }
-
-	   @Test
-	   public void givenMessageShouldReturnSadWhenTestIsPassed(){
-	      MoodAnalyser mood =  new MoodAnalyser();
-	      String testStatus = mood.messageContainHappyReturnHappyElseSad("This is Sad message");
-	      Assert.assertEquals("Sad",testStatus);
+	     boolean actualParameter = UserDetail.gmailTest(emailPattern, expectedResult);
+	      System.out.println("sum of numbers = : "+actualParameter);
+	      Assert.assertEquals(expectedResult,actualParameter);
 	   }
 	}
