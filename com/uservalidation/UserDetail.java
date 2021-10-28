10baaea9eed7f211dc47382d878d8a3a57b817d1
Smruti <lenkasmruti123@gmail.com>
2021-10-28 22:19:13 +0530

    Added: Uc3_ValidEmail

--- com/uservalidation/UserDetail.java
+++ com/uservalidation/UserDetail.java
@@ -2,11 +2,13 @@ package com.uservalidation;
 
 import java.util.regex.Pattern;
 
+
 public class UserDetail {
-    	public boolean lastName(String name) {
-            boolean mathPattern = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", name);
-            return mathPattern;
-        }
-    }
+	 public boolean gMail(String gmail) {
+	        System.out.println("Email Testing");
+	        boolean matchesString = Pattern.matches("^[A-Za-z._]+[@]{1}[a-zA-Z]+[.a-z]{4}([.a-zA-Z]{3})*$", gmail);
+	        return matchesString;
+	    }
+	}
     
     
--- com/uservalidation/UserDetailsTest.java
+++ com/uservalidation/UserDetailsTest.java
@@ -1,15 +1,15 @@
 package com.uservalidation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import junit.framework.Assert;
-
-import org.junit.jupiter.api.Assertions;
-
 public class UserDetailsTest {
-	 @Test
-	   public void givenNameWhenPropershouldReturnTrue(){
-		 UserDetailsTest userDetails = new UserDetailsTest();
-	         boolean testStatus = userDetails.lastName("Lenka");
+	 private Assertions Assert;
+
+	   @Test
+	   public void givenEmailWhenShouldReturnTrue(){
+		   UserDetailsTest userDetails = new UserDetailsTest();
+	         boolean testStatus = userDetails.gMail("smruti.lenka@gsdg.com.in");
 	         Assert.assertTrue(testStatus);
 	   }
+}
